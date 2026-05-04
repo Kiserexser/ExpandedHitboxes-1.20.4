997c16518a46a716c4dd2efe232e5e80e57960be
Kiserexser <towartors@mail.ru>
2026-05-04 19:12:38 +0500

    Create LivingEntityMixin.java

---
+++ src/main/java/com/swill/hitbox/src/main/java/com/swill/hitbox/src/main/java/com/swill/hitbox/mixin/src/main/java/com/swill/hitbox/mixin/LivingEntityMixin.java
@@ -0,0 +1,23 @@
+package com.swill.hitbox.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source.getAttacker() instanceof PlayerEntity player) {
+            LivingEntity target = (LivingEntity)(Object)this;
+            double distanceSq = player.squaredDistanceTo(target);
+            if (distanceSq > 9.0) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
